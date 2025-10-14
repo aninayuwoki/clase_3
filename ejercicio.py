875595ca352125f08b2982fdf2ba34638ff3eaae
aninayuwoki <christhianrivera814@gmail.com>
2025-10-14 18:19:04 -0500

    hice una lista de mis materias

--- ejercicio.py
+++ ejercicio.py
@@ -1,1 +1,3 @@
 print("hola, Como lo llevas mi bro xd xd ")
+
+ciclo = ["Programacion orientada a objetos", "bases de datos II", "matematicas aplicadas II", "diseño multimedia ", "metodologia del desarrollo de software"]
