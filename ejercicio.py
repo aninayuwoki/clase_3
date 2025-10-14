1e4ee7d18626dba7a57e8ddbdf572dac2dfa2df7
aninayuwoki <christhianrivera814@gmail.com>
2025-10-14 18:26:52 -0500

    creacion de la lista de notas

--- ejercicio.py
+++ ejercicio.py
@@ -1,3 +1,4 @@
 print("hola, Como lo llevas mi bro xd xd ")
 
 ciclo = ["Programacion orientada a objetos", "bases de datos II", "matematicas aplicadas II", "diseño multimedia ", "metodologia del desarrollo de software"]
+notas = [10, 10, 10, 10, 10]
